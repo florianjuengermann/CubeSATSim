18d9d9d2e994836290f16d53feaefdd171fa1cb3
maxvonwolff <max@maxvw.de>
2017-06-30 23:34:25 +0200

    update 1

---
+++ Main.m
@@ -0,0 +1,99 @@
+clear variables;
+<<<<<<< HEAD
+=======
+%florians branch
+>>>>>>> 1900c4c4cd7d72f5119bd73e1dd4e8f00ae7dee5
+
+%--------- GLOBAL CONSTANTS----------------
+global MU_0 GAMMA EARTH_RADIUS EARTH_MASS T;
+EARTH_RADIUS = 6371000;
+EARTH_MASS = 5.972e24;
+GAMMA = 6.674e-11;
+MU_0 = pi*4e-7;
+T = 1;
+
+% $x^2+e^{\pi i}$
+%-------- CUBESAT PARAMETERS--------------
+global HEIGHT J CUBE_MASS;
+HEIGHT = 400000;
+J = [ [1.0/600, 0, 0]; [ 0, 1.0/600, 0]; [0, 0, 1.0/600]]; 
+CUBE_MASS = 1;
+
+
+
+V0 = sqrt(GAMMA * EARTH_MASS / EARTH_RADIUS)
+
+dipoleEarth = [0,0, 1e23];
+dipoleCube = [1,0, 0]; %TODO test only
+
+posSAT = [EARTH_RADIUS+HEIGHT,0,0]; 
+veloSAT = [0, V0, 0];
+
+angularVel = [0, 0, 0];
+dirSAT = [0, 0, 0]
+
+B = mFluxDesity(posSAT, dipoleEarth);
+F_G = gravityEarth(posSAT, 1)
+F_m = magneticForce(posSAT, dipoleCube, dipoleEarth)
+
+
+while 1
+    
+    %------- CUBESAT POSITION -------
+    F_G = gravityEarth(posSAT, 1)
+    % TODO Calculate dipoleCube based on time-varying input
+    F_m = magneticForce(posSAT, dipoleCube, dipoleEarth)
+    
+    accSAT = (F_G + F_M) / CUBE_MASS
+    
+    veloSAT = veloSAT + accSAT * T
+    posSAT = posSAT + veloSAT * T
+    
+    
+    
+    %------- CUBESAT ATTITUDE -------
+    B = mFluxDesity(posSAT, dipoleEarth)
+    tSAT = magneticTorque(B, dipoleCube)
+    
+    angularAcc = J \ b; %inv(J) * tSAT
+    angularVel = angularVel + angularAcc * T
+    dirSAT = dirSAT 
+    
+end
+
+function F_G = gravityEarth(r, m)
+%   r: from earth's center to location
+%   m: mass of object
+    global EARTH_MASS GAMMA;
+    F_G = GAMMA * EARTH_MASS * m / (norm(r)^2) * r / (-norm(r));
+
+end
+
+
+function B = mFluxDesity(r, m )
+%   r: from magentic dipole m to location
+%   m: magnetic dipole momentum (Vector)
+    global MU_0;
+    B = MU_0* 1 / ( 4 * pi) * ( (3*r*dot(r,m)) / (norm(r)^5)  - m / (norm(r)^3) );
+
+end
+
+function F_m = magneticForce(r, m, mE)
+%   m2 : m   ,   m1: mE
+%   r: from earth's center to location
+%   m: magnetic dipole momentum of object
+%   mE: magnetic dipole momentum of the earth
+    global MU_0;
+    rh = r / norm(r);
+    F_m = 3 * MU_0 /( 4* pi * (norm(r)^4)) * ( m * dot(mE, rh) + mE * dot(m,r) + rh * dot(m, mE) - 5 * rh * dot(mE, rh) * dot(m, rh));
+end
+
+function t = magneticTorque(B, m)
+%   B: magnetic flux density
+%   m: magnetic dipole moment
+    t = cross(m, B);
+end
+
+function 
+
+
